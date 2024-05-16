feabe7b3b3e291a3a77c78d0b7e0c3f3c57098f5
Illia Rohozhkin <systemmind@ukr.net>
2024-05-16 12:51:50 +0300

    version 1.0

---
+++ config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  title: "Knot theory and related topics seminars",
+  description: "Knot theory and related topics seminars",
+  author: "miptctx",
+  domain: "http://ktrt-seminars.42web.io",
+  theme: {
+    default: "light"
+  }
+};
+
+export default config;
